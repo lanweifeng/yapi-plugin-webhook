d616a986952be5badf945ba65e3aa4870a00a960
lanweifeng <lanweifeng@chaintor.com>
2019-10-25 11:16:48 +0800

    增加了配置可选项

--- utils/monitor.js
+++ utils/monitor.js
@@ -1,20 +1,21 @@
 const axios = require('axios');
 const hooksMap = require('./events');
-
-//下划线转驼峰
-const toCamelCaseVar = (variable) =>
-  variable.replace(/\_+[a-zA-Z]/g,
-    (str,index) => index ? str.substr(-1).toUpperCase() : str
-  )
+const {toCamelCaseVar, getIPAdress} = require('./index');
 
 class Monitor{
-  constructor(options){
+  constructor(options, name){
     if(!options.url) {
       console.log('请配置钩子监听器的url地址!');
       return;
     }
+    //监听器名称
+    this.name = name || '';
     // 监听器地址
     this.url = options.url;
+    //yapi项目地址
+    this.yapiAdress = options.yapiAdress || getIPAdress();
+    //yapi项目端口
+    this.yapiPort = options.yapiPort;
     // 发送的方式
     this.method = (options.method || 'get').toLowerCase();
     // 是否启用
@@ -25,25 +26,38 @@ class Monitor{
     this.desc = options.desc || '';
 
     (options.events || []).forEach(event => {
-      console.log('this.events', this.events, event);
       this.bindEvent(event);
     });
   }
 
   // 发送消息
-  async sendMessage(message, hook){
-    axios.post
-    return await axios[this.method](this.url, {params: message})
-      .then(console.log(`事件${hook}发送到${this.url}成功！发送数据${message}`))
-      .catch(console.log(`事件${hook}发送到${this.url}失败！失败数据${message}`));
+  async sendMessage(sendData){
+    if(!sendData) return ;
+
+    return await axios({
+      method: this.method,
+      url: this.url,
+      data: sendData,
+    })
+      .then(res => {
+        console.log(`事件${sendData.event}发送到${this.url}成功！\n响应状态${res.status}`);
+        console.log('响应头', res.headers)
+      })
+      .catch(err => {
+        console.log(`事件${sendData.event}发送到${this.url}失败！\n失败报错${err}`);
+      });
   }
 
   // 注册事件绑定
   bindEvent(event){
     if(this[event]) return;
+    console.log(`监听器${this.name}绑定事件:${event}`);
     this[event] = hooksMap[toCamelCaseVar(event)];
     this[event].bind(this);
   }
+
 }
+
+
 
 module.exports = Monitor;
